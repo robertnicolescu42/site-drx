57dfb1b7bed419950df462944bfcced79d2cae9f
Robert Nicolescu <robertnicolescu42@yahoo.com>
2020-01-14 21:27:34 +0200

    v4 frontend pentru app2

---
+++ APP 2/sitemarin/index.php
@@ -0,0 +1,109 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/arta.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/brands.css" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/solid.css" rel="stylesheet">
+    <title>Rezervări</title>
+</head>
+<body>
+<!-- head-ul siteului minimalist-->
+<div class="container-fluid bg-dark">
+    <div class="row">
+        <div class="col"></div>
+         <div class="col ml-3 color-fa-home">
+                <span><i class="fas fa-home fa-3x"></i></span>
+         </div>
+    <div class="col"></div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col"></div>
+
+        <div class="col">
+            <div class="d-flex justify-content-center h-100">
+                <div class="card" style="height: 313px;">
+                    <div class="rezervare-curenta">
+                        <!-- id-ul rezervarii + nr participanti -->
+                        <div class="card-header bg-dark rezervari" style="margin: 15px 15px;">
+                            <h3 style="font-size: 16px">Camera: VR PIT 5 8 | maxim 8 participanti</h3>
+                        </div>
+                        <!-- rezervare curenta -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Rezervat pentru : Proiect migrare la Oracle 19c</h3> 
+                        </div>
+                         <!-- perioada -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Pana la: 13.11.2019 14:00</h3> 
+                        </div>                   
+                         <!-- organizator -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Organizator: Robert Nicolescu</h3> 
+                        </div>
+                         <!-- Participanti -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Participanti: Ion Ion; Mihai Jackson;</h3> 
+                        </div>
+                    </div>
+
+                    <div class="rezervarea-urmatoare">
+                        <!-- id-ul rezervarii + nr participanti -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Camera: VR PIT 5 8 | maxim 8 participanti</h3>
+                        </div>
+                        <!-- rezervare curenta -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Rezervat pentru : Proiect migrare la Oracle 19c</h3> 
+                        </div>
+                         <!-- perioada -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Pana la: 13.11.2019 14:00</h3> 
+                        </div>                   
+                         <!-- organizator -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Organizator: Robert Nicolescu</h3> 
+                        </div>
+                         <!-- Participanti -->
+                        <div class="card-header bg-dark">
+                            <h3 style="font-size: 16px">Participanti: Ion Ion; Mihai Jackson;</h3> 
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    <div class="col"></div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
